676d1feca012a29970e77413df60fbe8ba9d34e9
hungdt379 <hungdt379@gmail.com>
2021-06-20 13:47:40 +0700

    [Unit-Test]

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,12 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'prefix' => 'auth'
 ], function () {
-    Route::post('login', 'AuthController@login');
+    Route::post('login', 'AuthController@login')->name('login');
     Route::post('login/table', 'AuthController@loginForTable');
     Route::group([
         'middleware' => 'auth.jwt'
     ], function () {
-        Route::get('logout', 'AuthController@logout');
+        Route::get('logout', 'AuthController@logout')->name('logout');
     });
 });
 
@@ -36,7 +36,7 @@ Route::group([
 ], function () {
     Route::group(['middleware' => 'auth.jwt'], function () {
         Route::get('categories', 'CategoryController@getAllCategory');
-        Route::get('menu', 'MenuController@getMenu');
+        Route::get('menu', 'MenuController@getMenu')->name('menu');
         Route::get('menu/item/detail', 'MenuController@getDetailItem');
         Route::post('feedback', 'FeedbackController@addfeedback');
         Route::post('call/waiter', 'NotificationController@callWaiterNotification');
