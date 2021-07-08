bfda537ab9384a0f21fa4da5892828f54b82d275
hungdt379 <hungdt379@gmail.com>
2021-07-08 18:07:44 +0700

    seeding menu

--- app/Domain/Entities/Menu.php
+++ app/Domain/Entities/Menu.php
@@ -8,14 +8,14 @@ use Jenssegers\Mongodb\Eloquent\Model;
 
 class Menu extends Model
 {
-    const COMBO_129 = 'Combo nướng 129k';
+    const COMBO_129 = 'Combo nướng 139k';
     const COMBO_169 = 'Combo nướng 169k';
-    const COMBO_209 = 'Combo lẩu nướng 209k';
+    const COMBO_209 = 'Combo lẩu nướng 249k';
 
     protected $connection = 'mongodb';
     protected $collection = 'menu';
 
     protected $fillable = [
-        '_id', 'name', 'cost', 'description', 'image', 'hotpot', 'category_id'
+        '_id', 'name', 'cost', 'image', 'hotpot', 'category_id'
     ];
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //TODO: can change Role permission to using laravel packages
+        $this->call(Category::class);
+        $this->call(Menu::class);
+        $this->call(DishInCombo::class);
         $this->call(SearchCombo129::class);
         $this->call(SearchCombo169::class);
         $this->call(SearchCombo209::class);
