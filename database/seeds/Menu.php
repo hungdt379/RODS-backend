4f0e402898982879ebf5d70522e2ca2238416dc6
hungdt379 <hungdt379@gmail.com>
2021-07-12 18:09:26 +0700

    seeding menu

--- database/seeds/Menu.php
+++ database/seeds/Menu.php
@@ -27,7 +27,7 @@ class Menu extends Seeder
 
             'name' => 'Combo nướng 139k',
             'cost' => 139000,
-            'image' => 'http://165.227.99.160/image/nuong-129.png',
+            'image' => 'http://165.227.99.160/image/139.png',
             'category_id' => $combo->_id,
             'is_sold_out' => false
         ]);
@@ -35,7 +35,7 @@ class Menu extends Seeder
 
             'name' => 'Combo nướng 169k',
             'cost' => 169000,
-            'image' => 'http://165.227.99.160/image/nuong-169.png',
+            'image' => 'http://165.227.99.160/image/169.png',
             'category_id' => $combo->_id,
             'is_sold_out' => false
         ]);
@@ -43,7 +43,7 @@ class Menu extends Seeder
 
             'name' => 'Combo nướng 249k',
             'cost' => 249000,
-            'image' => 'http://165.227.99.160/image/nuong-169.png',
+            'image' => 'http://165.227.99.160/image/249.png',
             'category_id' => $combo->_id,
             'is_sold_out' => false
         ]);
@@ -52,7 +52,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Nầm heo',
             'cost' => 100000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/namheo.png',
             'category_id' => $normal->_id,
             'is_sold_out' => false
         ]);
@@ -60,7 +60,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Sụn heo',
             'cost' => 100000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/sunheo.png',
             'category_id' => $normal->_id,
             'is_sold_out' => false
         ]);
@@ -68,7 +68,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Ba chỉ bò',
             'cost' => 100000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/bachibo.png',
             'category_id' => $normal->_id,
             'is_sold_out' => false
         ]);
@@ -76,7 +76,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Ba chỉ heo',
             'cost' => 100000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/bachiheo.png',
             'category_id' => $normal->_id,
             'is_sold_out' => false
         ]);
@@ -84,7 +84,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Dẻ sườn',
             'cost' => 100000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/desuon.png',
             'category_id' => $normal->_id,
             'is_sold_out' => false
         ]);
@@ -92,7 +92,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Cổ bò',
             'cost' => 100000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/cobo.png',
             'category_id' => $normal->_id,
             'is_sold_out' => false
         ]);
@@ -100,7 +100,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Bò cuốn nấm',
             'cost' => 60000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/bocuonnam.png',
             'category_id' => $normal->_id,
             'is_sold_out' => false
         ]);
@@ -108,7 +108,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Tôm sú',
             'cost' => 100000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/tomsu.png',
             'category_id' => $normal->_id,
             'is_sold_out' => false
         ]);
@@ -116,7 +116,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Mực',
             'cost' => 100000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/muc.png',
             'category_id' => $normal->_id,
             'is_sold_out' => false
         ]);
@@ -124,7 +124,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Chân gà',
             'cost' => 100000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/changa.png',
             'category_id' => $normal->_id,
             'is_sold_out' => false
         ]);
@@ -133,7 +133,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Kimchi',
             'cost' => 10000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/kimchi.png',
             'category_id' => $fastFood->_id,
             'is_sold_out' => false
         ]);
@@ -141,7 +141,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Khoai tây chiên',
             'cost' => 30000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/khoaitaychien.png',
             'category_id' => $fastFood->_id,
             'is_sold_out' => false
         ]);
@@ -149,7 +149,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Bánh bao chiên',
             'cost' => 20000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/banhbaochien.png',
             'category_id' => $fastFood->_id,
             'is_sold_out' => false
         ]);
@@ -157,29 +157,22 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => 'Nem chua rán',
             'cost' => 30000,
-            'image' => 'http://165.227.99.160/image/nem-chua-ran.png',
+            'image' => 'http://165.227.99.160/image/nemchuaran.png',
             'category_id' => $fastFood->_id,
             'is_sold_out' => false
         ]);
 
         DB::table('menu')->insert([
-            'name' => 'Khoai lang kén',
-            'cost' => 30000,
-            'image' => 'http://165.227.99.160/image/khoai-lang-ken.png',
-            'category_id' => $fastFood->_id,
-            'is_sold_out' => false
-        ]);
-        DB::table('menu')->insert([
             'name' => 'Ngô chiên',
             'cost' => 30000,
-            'image' => 'http://165.227.99.160/image/ngo-chien.png',
+            'image' => 'http://165.227.99.160/image/ngochien.png',
             'category_id' => $fastFood->_id,
             'is_sold_out' => false
         ]);
         DB::table('menu')->insert([
-            'name' => 'Gà lắc',
+            'name' => 'Gà chiên mắm',
             'cost' => 50000,
-            'image' => 'http://165.227.99.160/image/ga-lac.png',
+            'image' => 'http://165.227.99.160/image/gachienmam.png',
             'category_id' => $fastFood->_id,
             'is_sold_out' => false
         ]);
@@ -188,7 +181,7 @@ class Menu extends Seeder
         DB::table('menu')->insert([
             'name' => '7Up',
             'cost' => 10000,
-            'image' => 'http://165.227.99.160/image/7-up.png',
+            'image' => 'http://165.227.99.160/image/7up.png',
             'category_id' => $drink->_id,
             'is_sold_out' => false
         ]);
@@ -209,11 +202,51 @@ class Menu extends Seeder
             'is_sold_out' => false
         ]);
 
+        DB::table('menu')->insert([
+            'name' => 'Lavie',
+            'cost' => 10000,
+            'image' => 'http://165.227.99.160/image/lavie.png',
+            'category_id' => $drink->_id,
+            'is_sold_out' => false
+        ]);
+
+        DB::table('menu')->insert([
+            'name' => 'Bò húc',
+            'cost' => 15000,
+            'image' => 'http://165.227.99.160/image/bohuc.png',
+            'category_id' => $drink->_id,
+            'is_sold_out' => false
+        ]);
+
+        DB::table('menu')->insert([
+            'name' => 'Trà Ô Long',
+            'cost' => 10000,
+            'image' => 'http://165.227.99.160/image/traolong.png',
+            'category_id' => $drink->_id,
+            'is_sold_out' => false
+        ]);
+
         /////bia
         DB::table('menu')->insert([
             'name' => 'Bia Hà Nội',
             'cost' => 15000,
-            'image' => 'http://165.227.99.160/image/bia.png',
+            'image' => 'http://165.227.99.160/image/hanoi.png',
+            'category_id' => $beer->_id,
+            'is_sold_out' => false
+        ]);
+
+        DB::table('menu')->insert([
+            'name' => 'Bia Sài Gòn',
+            'cost' => 15000,
+            'image' => 'http://165.227.99.160/image/saigon.png',
+            'category_id' => $beer->_id,
+            'is_sold_out' => false
+        ]);
+
+        DB::table('menu')->insert([
+            'name' => 'Bia Heniken',
+            'cost' => 20000,
+            'image' => 'http://165.227.99.160/image/heniken.png',
             'category_id' => $beer->_id,
             'is_sold_out' => false
         ]);
@@ -226,44 +259,59 @@ class Menu extends Seeder
             'is_sold_out' => false
         ]);
 
+        DB::table('menu')->insert([
+            'name' => 'Bia 333',
+            'cost' => 15000,
+            'image' => 'http://165.227.99.160/image/333.png',
+            'category_id' => $beer->_id,
+            'is_sold_out' => false
+        ]);
+
+        DB::table('menu')->insert([
+            'name' => 'Bia Tiger',
+            'cost' => 15000,
+            'image' => 'http://165.227.99.160/image/tiger.png',
+            'category_id' => $beer->_id,
+            'is_sold_out' => false
+        ]);
 
         //////rượu
         DB::table('menu')->insert([
             'name' => 'Rượu Soju Hàn Quốc',
             'cost' => 70000,
-            'image' => 'http://165.227.99.160/image/soju.png',
+            'image' => 'http://165.227.99.160/image/sochu.png',
             'category_id' => $alcohol->_id,
             'is_sold_out' => false
         ]);
 
         DB::table('menu')->insert([
-            'name' => 'Rượu chuối hột 500ml',
+            'name' => 'Rượu chuối hột',
             'cost' => 50000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/chuoihot.png',
             'category_id' => $alcohol->_id,
             'is_sold_out' => false
         ]);
 
         DB::table('menu')->insert([
-            'name' => 'Rượu ngô 500ml',
+            'name' => 'Rượu ngô',
             'cost' => 50000,
-            'image' => 'http://165.227.99.160/image/no-image.png',
+            'image' => 'http://165.227.99.160/image/ngo.png',
             'category_id' => $alcohol->_id,
             'is_sold_out' => false
         ]);
 
         DB::table('menu')->insert([
-            'name' => 'Rượu táo mèo 500ml',
+            'name' => 'Rượu táo mèo',
             'cost' => 50000,
-            'image' => 'http://165.227.99.160/image/tao-meo.png',
+            'image' => 'http://165.227.99.160/image/tao.png',
             'category_id' => $alcohol->_id,
             'is_sold_out' => false
         ]);
 
         DB::table('menu')->insert([
-            'name' => 'Rượu nếp trắng 500ml',
+            'name' => 'Rượu nếp',
             'cost' => 50000,
-            'image' => 'http://165.227.99.160/image/nep-trang.png',
+            'image' => 'http://165.227.99.160/image/nep.png',
             'category_id' => $alcohol->_id,
             'is_sold_out' => false
         ]);
