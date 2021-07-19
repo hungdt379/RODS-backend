47fc8860ee32332e99f425a12ad2cb980c562284
hungdt379 <hungdt379@gmail.com>
2021-07-19 15:13:00 +0700

    edit search

--- app/Domain/Entities/Menu.php
+++ app/Domain/Entities/Menu.php
@@ -10,7 +10,7 @@ class Menu extends Model
 {
     const COMBO_129 = 'Combo nướng 139k';
     const COMBO_169 = 'Combo nướng 169k';
-    const COMBO_209 = 'Combo lẩu nướng 249k';
+    const COMBO_209 = 'Combo nướng 249k';
 
     protected $connection = 'mongodb';
     protected $collection = 'menu';
